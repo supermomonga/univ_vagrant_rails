2bba474f47f86108fdf19c0416ecf15a5aae5e9e
supermomonga <hi@supermomonga.com>
2014-01-18 03:33:42 +0900

    initial commit

---
+++ railsapp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shopping::Application.config.secret_key_base = '257334c5218641c8bb09741650fa0ad06a3cc51fd51603ac5e48471be00f50286dbd87dd7a0dda0b4278e691b88e3478997eb788ef1f4d2975a0941f4f11db69'
