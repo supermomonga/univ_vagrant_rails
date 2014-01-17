eb7bb44112559a6fcfa8a1638b6f16821ed2ec2e
supermomonga <hi@supermomonga.com>
2014-01-18 04:54:47 +0900

    i18n settings

--- railsapp/app/controllers/application_controller.rb
+++ railsapp/app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_locale
+
+  private
+
+  def set_locale
+    extracted_locale = params[:locale] || extract_locale_from_accept_language
+    I18n.locale = I18n::available_locales.include?(extracted_locale) ? extracted_locale : I18n.default_locale
+  end
+
+  def extract_locale_from_accept_language
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first.to_sym
+  end
+
 end
--- railsapp/config/routes.rb
+++ railsapp/config/routes.rb
@@ -1,4 +1,7 @@
 Shopping::Application.routes.draw do
+  # get '/(:locale)' => 'welcome#index'
+  scope "/(:locale)", locale: /en|ja/ do
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
